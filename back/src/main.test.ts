dd96638325f7233068f67e624bd0182e2d918b3c
munxar <munxar@gmx.de>
2015-12-08 15:01:54 +0100

    added mocha tests and gulp typescript build.

---
+++ back/src/main.test.ts
@@ -0,0 +1,11 @@
+///<reference path="../../typings/tsd.d.ts"/>
+import * as chai from "chai";
+var expect = chai.expect;
+
+describe("test", () => {
+
+    it("wuxel 1", () => {
+        expect(1).eql(1);
+    })
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,28 @@
 var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var mocha = require("gulp-mocha");
 
-gulp.task("default", []);
+var config = {
+    back: {
+        build: "back:build",
+        test: "back:test",
+        files: "back/src/**/*.ts",
+        outDir: "back/dist",
+        testFiles: "back/dist/**/*.test.js",
+        tsConfig: {target: "es5", module: "commonjs"},
+        testConfig: {}
+    }
+};
+
+gulp.task(config.back.build, function () {
+    return gulp.src(config.back.files)
+        .pipe(ts(config.back.tsConfig))
+        .pipe(gulp.dest(config.back.outDir));
+});
+
+gulp.task(config.back.test, [config.back.build], function () {
+    return gulp.src(config.back.testFiles, {read: false})
+        .pipe(mocha(config.back.testConfig))
+});
+
+gulp.task("default", [config.back.test]);
